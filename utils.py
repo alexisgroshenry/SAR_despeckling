efa6c20c353d2da1647fdc81fafd26fde3bc8ce5
Alexis <alexis.groshenry@gmail.com>
2022-02-25 19:20:56 +0100

    typo on srt

--- utils.py
+++ utils.py
@@ -41,7 +41,7 @@ def load_train_data():
     datasetdir = './data/training/'
     # get the name of the piles for training (files must follow name convention "pilename_blabla.npy") # alexis
     filelist = glob(datasetdir+'*.npy') # alexis
-    name_pile = set([file.split('_')[0] for file in filelist]).sort() # alexis
+    name_pile = list(set([file.split('_')[0] for file in filelist])).sort() # alexis
     #name_pile = ['lely1', 'lely2', 'lely3', 'limagne1', 'limagne2', 'marais12', 'marais13'
     #name_pile = ['lely1', 'marais12', 'marais13'] 
     #name_pile = ['lely', 'marais1']
@@ -63,7 +63,7 @@ def load_train_data():
 def load_sar_images(datasetdir, pile): # alexis
     # get the name of the piles for evaluation (files must follow name convention "pilename_blabla.npy") # alexis
     filelist = glob(datasetdir+'*.npy') # alexis
-    name_pile = set([file.split('_')[0] for file in filelist]).sort() # alexis
+    name_pile = list(set([file.split('_')[0] for file in filelist])).sort() # alexis
     data = []
     eval_files = []
     for name_p in name_pile:
